33f18ad80f89d483ce8002d3f1a5a6683444e91f
Jared Booker <jay@tdm.io>
2015-12-28 13:06:36 -0400

    Config loader optional arg for config.json path

--- bin/utils/toolbox/config-loader.js
+++ bin/utils/toolbox/config-loader.js
@@ -3,10 +3,16 @@ var keyReader = require('./key-reader');
 var path = require('path');
 
 module.exports = function(type){
-	// Config path
-	var configPath = path.join(__TW, 'conf/', type + '.json');
-
-	// Try loading the default config location
+	var configPath = undefined;
+	var optPath = (arguments[1] !== undefined ? arguments[1][0] : undefined);
+	
+	// Default config location and name based off type
+	var defaultPath = path.join(__TW, 'conf/', type + '.json');
+	
+	// Optional override of configPath if arguments[1] is set.
+	// Set config path
+	configPath = (optPath === undefined ? defaultPath : optPath);
+	console.log('configPath',configPath)
 	// Wrapped in try to throw config loading error if files does not exist.
 	try {
 		var config = fs.readFileSync(configPath);
@@ -24,7 +30,7 @@ module.exports = function(type){
 			process.exit(0);
 		}
 		else{
-			console.log(error.red);
+			console.log('err', error.red);
 		}
 	}
 

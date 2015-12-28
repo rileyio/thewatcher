170518a07477205c2edbe7464ae445afc5eecee5
Jared Booker <jay@tdm.io>
2015-12-28 12:39:19 -0400

    Utils loader cleanup

--- bin/utils/utils.js
+++ bin/utils/utils.js
@@ -2,21 +2,32 @@
 // By #TheDoxMedia
 //
 
+// Universal/Toolbox (Not server or client specific)
 var configLoader = require('./toolbox/config-loader');
+var pgpMsgSign = require('./toolbox/sign');
+
+// Server specific
+var server_verify = require('./server/verify');
+var server_setup = require('./server/setup');
+var server_verifySig = require('./server/verify-sig');
+
+// Client specific
+var client_verifyServer = require('./client/verify-server');
+var client_setup = require('./client/setup');
 
 module.exports = {
 	client: {
-		setup: require('./client/setup'),
-		verifyServer: require('./client/verify-server'),
-		sign: require('./client/sign'),
+		setup: client_setup,
+		verifyServer: client_verifyServer,
+		sign: pgpMsgSign,
 		load: {
 			config: configLoader,
 		},
 	},
 	server: {
-		add: require('./server/verify'),
-		setup: require('./server/setup'),		
-		verifySig: require('./server/verify-sig'),		
+		add: server_verify,
+		setup: server_setup,		
+		verifySig: server_verifySig,		
 		load: {
 			config: configLoader,
 		},

9a0d9c2e51ac665e1eedec0b786d54b352c2a621
Jared Booker <jay@tdm.io>
2015-12-30 00:15:06 -0400

    Database charset set to utf8 on connection

--- bin/db/connection/connect.js
+++ bin/db/connection/connect.js
@@ -9,13 +9,14 @@ module.exports = function (config) {
 			host: config.host,
 			user: config.user,
 			password: config.pass,
-			database: config.database
+			database: config.database,
+			charset: 'utf8'
 		},
 		migrations: {
 			directory: path.join(__TW, 'migrations')
 		}
 	});
-	
+
 	connection.raw('SELECT 1+1 AS result')
 		.return() // Return if not any errors
 		.then(function () {
